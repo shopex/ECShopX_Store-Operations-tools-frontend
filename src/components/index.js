e34102d2bbbb1b22b4703a69c68886b637bb10c8
赵育坤 <19121152559@163.com>
2021-06-03 15:41:11 +0800

    fix:授权更改

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ export { default as SpTimer } from './sp-timer'
 export { default as SptitleBar } from './sp-title-bar'
 export { default as SpMessage } from './sp-message'
 export { default as SpRadio } from './sp-radio'
+export { default as SpafterSaleMessage } from './sp-afterSaleMessage'
 
 export default {}
