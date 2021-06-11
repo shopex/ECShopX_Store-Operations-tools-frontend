dc4c2012c5f3d071289851299bdbbdc76c763a5e
赵育坤 <19121152559@163.com>
2021-06-11 15:30:49 +0800

    fix:消息列表完善

--- src/pages/message/index.js
+++ src/pages/message/index.js
@@ -1,9 +1,9 @@
 import Taro from '@tarojs/taro'
 import React, { PureComponent } from 'react'
-
 import { View, Text, ScrollView } from '@tarojs/components'
 import { SptitleBar, SpMessage } from '@/components'
 import './index.scss'
+import { connect } from 'react-redux'
 
 import api from '@/api'
 
@@ -18,8 +18,6 @@ const SpMessageData = [
     },
     title: '售后订单',
     subtitle: '新增售后订单，请及时处理',
-    date: '5月18日',
-    messageNum: 111,
     onclickHander: function () {
       console.log('售后订单')
     }
@@ -31,8 +29,6 @@ const SpMessageData = [
     },
     title: '代发货订单',
     subtitle: '新增发货订单，请及时处理',
-    date: '5月19日',
-    messageNum: 13,
     onclickHander: function () {
       console.log('代发货订单')
     }
@@ -44,32 +40,87 @@ const SpMessageData = [
     },
     title: '未妥投订单',
     subtitle: '新增发货订单，请及时处理',
-    date: '5月20日',
-    messageNum: 12,
     onclickHander: function () {
       console.log('未妥投订单')
     }
   }
 ]
+
+@connect(({ planSelection }) => ({
+  planSelection: planSelection.activeShop
+}))
 export default class Message extends PureComponent {
   constructor() {
     super()
+    this.state = {
+      messageData: {}
+    }
   }
   componentDidMount() {
-    // console.log(api.message.getMessageList());
+    this.getConfig()
+  }
+  // 滑动事件
+  onscrollPage(e) {
+    console.log(e.detail.scrollTop)
+  }
+
+  async getConfig() {
+    let { distributor_id } = this.props.planSelection
+    distributor_id = distributor_id ? distributor_id : '102'
+    const result = await api.message.getMessageList({ distributor_id })
+    console.log(result)
+    this.setState({
+      messageData: result
+    })
+    console.log(this.state.messageData)
   }
 
   render() {
+    const { date_type_1, date_type_2, date_type_3, num_type_1, num_type_2, num_type_3 } =
+      this.state.messageData
     return (
       <View className='page-message'>
         {/* <View className='top'>
           <SptitleBar SpTitleBarData={SpTitleBarData}></SptitleBar>
         </View> */}
-        <ScrollView className='message-list'>
-          {SpMessageData.map((item) => {
-            return <SpMessage SpMessageData={item} key={item.title}></SpMessage>
-          })}
-        </ScrollView>
+
+        {this.state.messageData && (
+          <ScrollView className='message-list' onScroll={(e) => this.onscrollPage(e)}>
+            {SpMessageData.map((item, index) => {
+              if (index == 0) {
+                return (
+                  <SpMessage
+                    SpMessageData={item}
+                    date={date_type_1}
+                    messageNum={num_type_1}
+                    key={item.title}
+                  ></SpMessage>
+                )
+              } else if (index == 1) {
+                return (
+                  <SpMessage
+                    date={date_type_2}
+                    messageNum={num_type_2}
+                    SpMessageData={item}
+                    key={item.title}
+                  ></SpMessage>
+                )
+              } else {
+                return (
+                  <SpMessage
+                    date={date_type_3}
+                    messageNum={num_type_3}
+                    SpMessageData={item}
+                    key={item.title}
+                  ></SpMessage>
+                )
+              }
+              // {
+              //   index == 0  && return (<SpMessage SpMessageData={item} key={item.title}></SpMessage>)
+              // }
+            })}
+          </ScrollView>
+        )}
       </View>
     )
   }
