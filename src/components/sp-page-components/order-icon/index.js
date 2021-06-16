913a43967eaa9c895683d2384abf457f3ed2e3cc
wjb <wjb@wjbdeMacBook-Pro.local>
2021-06-16 17:17:00 +0800

    api修改

--- src/components/sp-page-components/order-icon/index.js
+++ src/components/sp-page-components/order-icon/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import { View, Text } from '@tarojs/components'
 import { classNames } from '@/utils'
-import { ORDER_DETAIL_ICON } from '@/consts'
+import { ORDER_DETAIL_ICON, afterSales } from '@/consts'
 import './index.scss'
 
 export default class OrderIcon extends PureComponent {
@@ -10,7 +10,10 @@ export default class OrderIcon extends PureComponent {
   }
 
   renderIcon = () => {
-    const { status } = this.props
+    const { status, pageType } = this.props
+    if (pageType === 'afterSalesDetail') {
+      return afterSales.DETAIL_ICON[String(status)]
+    }
     return ORDER_DETAIL_ICON[status]
   }
 
