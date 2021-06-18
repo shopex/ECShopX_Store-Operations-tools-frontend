1084c410ccd86b24dd85320c1ed31ae0a05c4bb1
赵育坤 <19121152559@163.com>
2021-06-18 10:46:13 +0800

    fix:优化售后

---
+++ src/pages/address/addAddress/index.js
@@ -0,0 +1,30 @@
+import { Component, createElement } from 'react'
+import { View, Image, Form, Input, Button, Picker, ScrollView } from '@tarojs/components'
+import { showToast } from '@/utils'
+import api from '@/api'
+import { SpAddress, SpEditForm } from '@/components'
+import './index.scss'
+
+import Taro from '@tarojs/taro'
+
+export default class Add_Address extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  componentDidMount() {
+    // this.getConfig()
+  }
+
+  notUpdate(message) {
+    showToast(`${message}暂不支持修改！`)
+  }
+
+  render() {
+    return (
+      <View className='page-addAddress'>
+        <SpEditForm seletedAddress={this.seletedAddress}></SpEditForm>
+      </View>
+    )
+  }
+}
