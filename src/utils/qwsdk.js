fcf3560a820243c46606a7bca0198e975968cd7d
wangzhanyuan <dreamworks.cnn@gmail.com>
2022-01-04 18:35:23 +0800

    fix

--- src/utils/qwsdk.js
+++ src/utils/qwsdk.js
@@ -75,12 +75,12 @@ class QWSDK {
     })
     console.log('QWSDK:register:jssdkConfig2', jssdkConfig)
 
-    const { appId, timestamp, nonceStr, signature, corpid } = jssdkConfig
+    const { appId, timestamp, nonceStr, signature } = jssdkConfig
     // eslint-disable-next-line no-undef
     wx.config({
       beta: true, // 必须这么写，否则wx.invoke调用形式的jsapi会有问题
       debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-      appId: corpid, // 必填，企业微信的corpID
+      appId, // 必填，企业微信的corpID
       timestamp, // 必填，生成签名的时间戳
       nonceStr, // 必填，生成签名的随机串
       signature, // 必填，签名，见 附录-JS-SDK使用权限签名算法
