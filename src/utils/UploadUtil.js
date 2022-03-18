4ad2198fa51abd50a6f2c6f9ad65810af7e82a3c
wjb <wjb@wjbdeMac-mini.local>
2022-03-18 11:50:53 +0800

    push tag

--- src/utils/UploadUtil.js
+++ src/utils/UploadUtil.js
@@ -124,7 +124,7 @@ class UploadUtil {
 
   // 上传
   async uploadImg(file) {
-    console.log('push tag')
+    console.log('push a tag')
     // 初始化
     try {
       const tokenRes = await getOssToken({ filetype: this.fileType })
