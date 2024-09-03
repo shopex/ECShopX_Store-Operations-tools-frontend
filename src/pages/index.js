8ebf0388c0d2cd5e22db7dd8b20d19c212e31183
chenxin <398275325@qq.com>
2024-09-03 11:44:22 +0800

    显示业务员管理

--- src/pages/index.js
+++ src/pages/index.js
@@ -418,7 +418,7 @@ class Index extends Component {
                   <View className='subtitle'>配送管理</View>
                 </View>
               )}
-              {/* {VERSION_STANDARD && is_salesman && (
+              {VERSION_STANDARD && is_salesman && (
                 <View
                   className='item'
                   onClick={() => {
@@ -433,7 +433,7 @@ class Index extends Component {
                   </View>
                   <View className='subtitle'>业务员管理</View>
                 </View>
-              )} */}
+              )}
               {VERSION_STANDARD && (
                 <View
                   className='item'
